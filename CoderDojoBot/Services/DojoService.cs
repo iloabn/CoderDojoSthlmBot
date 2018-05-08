299e7f48bbdeb5b1dec5a9a32e4ce34f028f96b8
Fabian Miiro <fabian.miiro@outlook.com>
2018-05-08 10:49:44 +0200

    Converted while into do...while

--- CoderDojoBot/Services/DojoService.cs
+++ CoderDojoBot/Services/DojoService.cs
@@ -27,11 +27,11 @@ namespace CoderDojoBot.Services
 
             var wednesdayStart = new DateTime(2018, 04, 11, 17, 30, 0);
 
-            while (wednesdayStart < new DateTime(2018, 06, 08))
+            do
             {
                 wednesdayStart = wednesdayStart.AddDays(7);
                 dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = wednesdayStart, End = wednesdayStart.AddHours(1.5) });
-            }
+            } while (wednesdayStart < new DateTime(2018, 06, 08));
 
             return dojos.OrderBy(dojo => dojo.Start).ToList();
         }
