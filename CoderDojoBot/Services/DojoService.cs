ba9ee14e13a7f90d64b646bf1e67bfbc2a54d11f
Fabian Miiro <fabian.miiro@outlook.com>
2018-09-05 22:11:59 +0200

    Add the first round of HT18 Dojos

--- CoderDojoBot/Services/DojoService.cs
+++ CoderDojoBot/Services/DojoService.cs
@@ -12,27 +12,24 @@ namespace CoderDojoBot.Services
         {
             var dojos = new List<Dojo>()
             {
-                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,02,03,10,0,0)},
-                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,02,10,10,0,0)},
-                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "ÅF", Start = new DateTime(2018,03,10,10,0,0)},
-                new Dojo{ Adress = "Sveavägen 46", Company = "Klarna", Start = new DateTime(2018,03,17,10,0,0)},
-                new Dojo{ Adress = "Sveavägen 46", Company = "Klarna", Start = new DateTime(2018,03,24,10,0,0)},
-                new Dojo{ Adress = "Drottninggatan 71D", Company = "iStone", Start = new DateTime(2018,04,14,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,04,21,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,04,28,10,0,0)},
-                new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,05,05,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,05,12,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,05,19,10,0,0)},
-                new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,05,26,10,0,0)},
+                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,08,10,0,0)},
+                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,15,10,0,0)},
+                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,06,10,0,0)},
+                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,13,10,0,0)},
+                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,20,10,0,0)},
             };
 
-            var wednesdayStart = new DateTime(2018, 04, 11, 17, 30, 0);
+            dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = new DateTime(2018, 09, 12, 17, 30, 0), End = new DateTime(2018, 09, 12, 17, 30, 0).AddHours(1.5)});
+            dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = new DateTime(2018, 09, 19, 17, 30, 0), End = new DateTime(2018, 09, 12, 17, 30, 0).AddHours(1.5)});
 
-            do
-            {
-                wednesdayStart = wednesdayStart.AddDays(7);
-                dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = wednesdayStart, End = wednesdayStart.AddHours(1.5) });
-            } while (wednesdayStart < new DateTime(2018, 06, 08));
+
+            //var wednesdayStart = new DateTime(2018, 04, 11, 17, 30, 0);
+
+            //do
+            //{
+            //    wednesdayStart = wednesdayStart.AddDays(7);
+            //    dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = wednesdayStart, End = wednesdayStart.AddHours(1.5) });
+            //} while (wednesdayStart < new DateTime(2018, 06, 08));
 
             return dojos.OrderBy(dojo => dojo.Start).ToList();
         }
