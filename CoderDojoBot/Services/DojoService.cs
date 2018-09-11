866ed18988b2cc2bcfbcb363b6a03d3db2b92736
Fabian Miiro <fabian.miiro@outlook.com>
2018-09-11 21:06:56 +0200

    Add Active Solution dates

--- CoderDojoBot/Services/DojoService.cs
+++ CoderDojoBot/Services/DojoService.cs
@@ -18,12 +18,13 @@ namespace CoderDojoBot.Services
                 new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,13,10,0,0)},
                 new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,20,10,0,0)},
                 new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,11,10,10,0,0)},
+                new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,17,10,0,0)},
+                new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,24,10,0,0)},
                 new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,12,01,10,0,0)},
             };
 
             dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = new DateTime(2018, 09, 12, 17, 30, 0), End = new DateTime(2018, 09, 12, 17, 30, 0).AddHours(1.5)});
             dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = new DateTime(2018, 09, 19, 17, 30, 0), End = new DateTime(2018, 09, 12, 17, 30, 0).AddHours(1.5)});
-
 
             //var wednesdayStart = new DateTime(2018, 04, 11, 17, 30, 0);
 
