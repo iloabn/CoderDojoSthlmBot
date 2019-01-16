4a34d0a9fa7ace446ac4d871a80245612da4eba2
Fabian Miiro <fabian.miiro@outlook.com>
2019-01-16 15:17:53 +0100

    Add Nordea dates

--- CoderDojoBot/Services/DojoService.cs
+++ CoderDojoBot/Services/DojoService.cs
@@ -11,17 +11,19 @@ namespace CoderDojoBot.Services
         {
             var dojos = new List<Dojo>()
             {
-                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,08,10,0,0)},
-                new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,15,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,06,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,13,10,0,0)},
-                new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,20,10,0,0)},
-                new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,11,10,10,0,0)},
-                new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,17,10,0,0)},
-                new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,24,10,0,0)},
-                new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,12,01,10,0,0)},
-                new Dojo{ Adress = "Frösundaleden 2", Company = "ÅF", Start = new DateTime(2018,12,08,10,0,0)},
-                new Dojo{ Adress = "Frösundaleden 2", Company = "ÅF", Start = new DateTime(2018,12,15,10,0,0)},
+                //new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,08,10,0,0)},
+                //new Dojo{ Adress = "Östermalmsgatan 26A", Company = "Iteam", Start = new DateTime(2018,09,15,10,0,0)},
+                //new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,06,10,0,0)},
+                //new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,13,10,0,0)},
+                //new Dojo{ Adress = "Årstaängsvägen 21B, 5tr", Company = "Cisco", Start = new DateTime(2018,10,20,10,0,0)},
+                //new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,11,10,10,0,0)},
+                //new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,17,10,0,0)},
+                //new Dojo{ Adress = "Kungstensgatan 23", Company = "Active Solution", Start = new DateTime(2018,11,24,10,0,0)},
+                //new Dojo{ Adress = "Drottninggatan 95A", Company = "Dynabyte", Start = new DateTime(2018,12,01,10,0,0)},
+                //new Dojo{ Adress = "Frösundaleden 2", Company = "ÅF", Start = new DateTime(2018,12,08,10,0,0)},
+                //new Dojo{ Adress = "Frösundaleden 2", Company = "ÅF", Start = new DateTime(2018,12,15,10,0,0)},
+                new Dojo { Adress = "TBA", Company = "Nordea", Start = new DateTime(2019,02,02,10,0,0) },
+                new Dojo { Adress = "TBA", Company = "Nordea", Start = new DateTime(2019,03,16,10,0,0) }
             };
 
             dojos.Add(new Dojo { Adress = "Hammarby Kaj 10D", Company = "FooCafe", Start = new DateTime(2018, 09, 12, 17, 30, 0), End = new DateTime(2018, 09, 12, 17, 30, 0).AddHours(1.5) });
